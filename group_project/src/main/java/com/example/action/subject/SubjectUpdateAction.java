152d75386b5c31bf8ca85cf736d71369c241ec1c
hanabusa <kmm2559330@stu.o-hara.ac.jp>
2026-04-22 12:15:59 +0900

    はなぶさ

---
+++ group_project/src/main/java/com/example/action/subject/SubjectUpdateAction.java
@@ -0,0 +1,26 @@
+package com.example.action.subject;
+
+import com.example.dao.SubjectDao;
+import com.example.action.Action;
+import com.example.model.Teacher;
+import com.example.model.Subject;
+import jakarta.servlet.http.*;
+
+public class SubjectUpdateAction implements Action {
+    public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
+
+        HttpSession session = req.getSession();
+
+        Teacher user = (Teacher)session.getAttribute("user");
+        
+        String code = req.getParameter("code");
+
+        SubjectDao dao = new SubjectDao();
+        Subject subject = dao.get(user.getSchoolCd(), code);
+        boolean line = dao.save(subject);
+
+        session.setAttribute("line", line);
+
+        req.getRequestDispatcher("/WEB-INF/views/subject/subject_update.jsp").forward(req, res);
+    }
+}

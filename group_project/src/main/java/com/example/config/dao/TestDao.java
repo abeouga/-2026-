f5b8645d16ed4e520bca7ad523c14ba193e36974
kudou <kmm2559320@stu.o-hara.ac.jp>
2026-04-27 10:12:12 +0900

    TestDao修正

--- group_project/src/main/java/com/example/config/dao/TestDao.java
+++ group_project/src/main/java/com/example/config/dao/TestDao.java
@@ -1,19 +1,29 @@
 package com.example.dao;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 
 import com.example.model.Test;
 import com.example.model.School;
 import com.example.model.Student;
 import com.example.model.Subject;
 
-public class TestDao extends DAO {
+public class TestDao {
 
     private String basesql =
         "select * from test where class_num = ? and subject_cd = ?";
+
+    private Connection getConnection() throws Exception {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        return DriverManager.getConnection(
+            "jdbc:mysql://localhost:3306/student_management",
+            "root",
+            "password"
+        );
+    }
 
     public Test get(Student student, Subject subject, School school, int no) throws Exception {
 

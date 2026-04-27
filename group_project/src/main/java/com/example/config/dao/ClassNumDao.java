96c2c04374fb31d0fc1e6ab4cebee2dca88760bc
kudou <kmm2559320@stu.o-hara.ac.jp>
2026-04-27 09:46:33 +0900

    工藤

---
+++ group_project/src/main/java/com/example/config/dao/ClassNumDao.java
@@ -0,0 +1,27 @@
+package com.example.dao;
+
+import com.example.model.ClassNum;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClassNumDao extends DaoBase {
+
+    public List<String> filter(String schoolCd) throws Exception {
+        List<String> list = new ArrayList<>();
+        String sql = "SELECT class_num FROM class_num WHERE school_cd = ? ORDER BY class_num";
+        try (Connection con = getConnection();
+             PreparedStatement st = con.prepareStatement(sql)) {
+            
+            st.setString(1, schoolCd);
+            try (ResultSet rs = st.executeQuery()) {
+                while (rs.next()) {
+                    list.add(rs.getString("class_num"));
+                }
+            }
+        }
+        return list;
+    }
+}
